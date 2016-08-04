e046373e1a7e4e45df3c1b50be2ccf7229b5d995
Sean Capelle <seancapelle@gmail.com>
2016-08-03 22:31:14 -0400

    Added pseudo code ideas

---
+++ assets/javascript/app.js
@@ -0,0 +1,36 @@
+//Variables 
+
+//"Page one" (index) variables
+//countryPick
+//statePick
+//moviePick
+//queryURL
+
+//"Page two" (list) variables
+//response.data
+//locationPic
+
+//"Page three" (movie) variables
+//posterImage
+//plot
+//trailer
+//movieName
+
+
+//Functions
+
+//"Page one" (index) functions
+//onClick to submit 
+	//get .val() input
+	//Ajax call
+
+//"Page two" (list) functions
+//Push movie titles to a "well"
+//Display locationPic
+//onClick to select moviePick
+	//Ajax call
+
+//"Page three" (movie) functions
+//Push poster and trailer variables to a div
+//Display locationPic
+
