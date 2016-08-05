cc0963daee1a79d353953f44aab62a787fa56936
Sean Capelle <seancapelle@gmail.com>
2016-08-04 21:30:57 -0400

    Added API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,17 +18,68 @@
 
 
 //Functions
+//Search by location
+var locationSearch = function(){
+var locationPick = "Delaware".split(' ').join('+');
+var queryURL = "http://www.filmaps.com/xml?loc=baltimore";
 
-var queryURL = "http://api.myapifilms.com/imdb/idIMDB?title=matrix&token=66a93a66-d0a3-4bcf-b682-329ad22dc9c3";
-			   console.log(queryURL)
-
-$.ajax({url: queryURL, method: 'GET'})
+	$.ajax({url: queryURL, method: 'GET'})
+	
 		.done(function(response) {
 
-		console.log(response);
+			console.log(response);
 		
-	
-		});
+	});
+
+};
+
+//Search by movie
+var movieSearch = function(){
+var moviePick = "batman".split(' ').join('+'); //#moviePick.val();
+var queryURL = "http://api.myapifilms.com/imdb/idIMDB?title=" + moviePick + "&token=66a93a66-d0a3-4bcf-b682-329ad22dc9c3";
+
+//Ajax call
+$.ajax({url: queryURL, method: 'GET'})
+	//Ajax response
+	.done(function(response) {
+
+		console.log(response);
+
+		//Create movie object
+		var movie = {
+			title: response.data.movies[0].title,
+			year: response.data.movies[0].year,
+			rating: response.data.movies[0].rated,
+			plot: response.data.movies[0].simplePlot,
+			poster: response.data.movies[0].urlPoster, 
+		};
+
+		//Push movie properties
+		//movie.title
+		//movie.year
+		//movie.rating
+		//movie.plot
+		//movie.poster
+		
+	});
+}
+
+// locationSearch();
+movieSearch();
+
+//object { "Guatemala":[], "Brazil": []   }
+//data.movies[0].filmingLocations[1]
+
+var places = { "Guatemala": ["star wars", "batman"] };
+
+//data.movies[0].filmingLocations[1]   //Guatemala
+
+//if object key contains Guatemala skip and just add originalTitle to its array
+//if object key doesn't contain Guatemala (and initialize as array object), add it as a key and at originalTitle to its array
+
+
+
+//if Guatamal  ( txtbox.val() if that is in places object(any of the keys) then print out that array)
 
 
 
@@ -45,5 +96,22 @@ $.ajax({url: queryURL, method: 'GET'})
 
 //"Page three" (movie) functions
 //Push poster and trailer variables to a div
+//Get poster
+var posterPush = function(){
+
+moviePick = "batman";	
+var queryURL = "http://www.omdbapi.com/?t=" + moviePick + "&y=&plot=short&r=json";
+
+	$.ajax({url: queryURL, method: 'GET'})
+		.done(function(response) {
+			
+			var poster = response.object.poster;
+		
+	});
+
+
+};
+posterPush();
+
 //Display locationPic
 
